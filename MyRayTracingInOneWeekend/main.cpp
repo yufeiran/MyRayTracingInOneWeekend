24c38452ccc0dcfeada1a11af44dc26f2414ed51
yufeiran <yufeiran1999@gmail.com>
2022-08-21 14:07:22 +0800

    完成第8节 Diffuse Materials

--- MyRayTracingInOneWeekend/main.cpp
+++ MyRayTracingInOneWeekend/main.cpp
@@ -3,6 +3,7 @@
 #include"color.h"
 #include"hittable_list.h"
 #include"sphere.h"
+#include"camera.h"
 
 
 #include<iostream>
@@ -23,10 +24,16 @@ double hit_sphere(const point3& center, double radius, const ray& r) {
 
 }
 
-color ray_color(const ray& r,const hittable&world) {
+color ray_color(const ray& r,const hittable&world,int depth) {
 	hit_record rec;
-	if (world.hit(r, 0, infinity, rec)) {
-		return 0.5 * (rec.normal + color(1, 1, 1));
+
+	//超过迭代次数时终止
+	if (depth <= 0)
+		return color(0, 0, 0);
+
+	if (world.hit(r, 0.001, infinity, rec)) {
+		point3 target = rec.p + rec.normal + random_in_hemisphere(rec.normal);
+		return 0.5 * ray_color(ray(rec.p,target-rec.p),world,depth-1);
 	}
 
 	vec3 unit_direction = unit_vector(r.direction());
@@ -41,6 +48,8 @@ int main() {
 	const auto aspect_ratio = 16.0 / 9.0;
 	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width/aspect_ratio);
+	const int samples_per_pixel = 100;
+	const int max_depth = 50;
 
 	// World
 	hittable_list world;
@@ -48,31 +57,25 @@ int main() {
 	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
 	// Camera
-
-	auto viewport_height = 2.0;
-	auto viewport_width = aspect_ratio * viewport_height;
-	auto focal_length = 1.0;
-
-	auto origin = point3(0, 0, 0);
-	auto horizontal = vec3(viewport_width, 0, 0);
-	auto vertical = vec3(0, viewport_height, 0);
-	auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
-
+	camera cam;
 
 
 	// Render
 
 	std::ofstream os(std::string("out.ppm"));
-	os<< "p3\n" << image_width << ' ' << image_height << "\n255\n";
+	os<< "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 	for (int j = image_height - 1; j >= 0; --j) {
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; ++i) {
-			auto u = double(i) / (image_width - 1);
-			auto v = double(j) / (image_height - 1);
-			ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-			color pixel_color = ray_color(r,world);
-			write_color(os, pixel_color);
+			color pixel_color(0, 0, 0);
+			for (int s = 0; s < samples_per_pixel; ++s) {
+				auto u = double(i+random_double()) / (image_width - 1);
+				auto v = double(j+random_double()) / (image_height - 1);
+				ray r = cam.get_ray(u, v);
+				pixel_color += ray_color(r, world,max_depth);
+			}
+			write_color(os, pixel_color,samples_per_pixel);
 		}
 	}
 	std::cerr << "\nDone.\n";
