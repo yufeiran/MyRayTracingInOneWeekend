552081336f39cde22dbb10cd572f23925a108d9c
yufeiran <yufeiran1999@gmail.com>
2022-08-22 09:39:58 +0800

    完成第9章 Metal

--- MyRayTracingInOneWeekend/vec3.h
+++ MyRayTracingInOneWeekend/vec3.h
@@ -38,6 +38,12 @@ public:
 		return *this *= 1 / t;
 	}
 
+	bool near_zero() const {
+		// Return true if the vector is close to zero in all dimensions.
+		const auto s = 1e-8;
+		return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
+	}
+
 	double length() const {
 		return sqrt(length_squared());
 	}
@@ -123,6 +129,9 @@ vec3 random_in_hemisphere(const vec3& normal) {
 		return -in_unit_sphere;
 }
 
+vec3 reflect(const vec3& v, const vec3& n) {
+	return v - 2 * dot(v, n) * n;
+}
 
 
 using point3 = vec3; //3D point
