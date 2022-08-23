4e93f6dfe803644c563e58768c6a00a1b4727186
yufeiran <yufeiran1999@gmail.com>
2022-08-23 09:08:29 +0800

    完成第11章 Positionable Camera

--- MyRayTracingInOneWeekend/rtweekend.h
+++ MyRayTracingInOneWeekend/rtweekend.h
@@ -21,7 +21,7 @@ const double pi = 3.1415926535897932385;
 // Utility Functions
 
 inline double degrees_to_radians(double degrees) {
-	return degrees * pi / 180 / 0;
+	return degrees * pi / 180.0;
 }
 
 inline double random_double() {
