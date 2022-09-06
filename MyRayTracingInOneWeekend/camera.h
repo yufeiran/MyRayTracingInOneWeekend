5c08ff10ada99c9424e5ea321d20c5e47966bbab
yufeiran <yufeiran1999@gmail.com>
2022-09-06 09:10:09 +0800

    进入Ray Tracing: The Next Week，完成第2节Motion Blur

--- MyRayTracingInOneWeekend/camera.h
+++ MyRayTracingInOneWeekend/camera.h
@@ -14,7 +14,9 @@ public:
 		double vfov,  // vertical field-of-view in degrees
 		double aspect_ratio,
 		double aperture,
-		double focus_dist
+		double focus_dist,
+		double _time0=0,
+		double _time1=0
 	) {
 		auto theta = degrees_to_radians(vfov);
 		auto h = tan(theta / 2);
@@ -32,6 +34,9 @@ public:
 		lower_left_corner=origin - horizontal / 2 - vertical / 2 -focus_dist* w;
 
 		lens_radius = aperture / 2;
+		time0 = _time0;
+		time1 = _time1;
+		
 	}
 
 	ray get_ray(double s, double t)const {
@@ -39,7 +44,8 @@ public:
 		vec3 offset = u * rd.x() + v * rd.y();
 
 		return ray(origin+offset, 
-			lower_left_corner + s * horizontal + t * vertical - origin-offset
+			lower_left_corner + s * horizontal + t * vertical - origin-offset,
+			random_double(time0,time1)
 		);
 	}
 
@@ -50,6 +56,7 @@ private:
 	vec3 vertical;
 	vec3 u, v, w;
 	double lens_radius;
+	double time0, time1; // shutter open/close times
 };
 
 #endif //!CAMERA_H
--- MyRayTracingInOneWeekend/main.cpp
+++ MyRayTracingInOneWeekend/main.cpp
@@ -5,6 +5,7 @@
 #include"sphere.h"
 #include"camera.h"
 #include"material.h"
+#include"moving_sphere.h"
 
 
 #include<iostream>
@@ -64,7 +65,9 @@ hittable_list random_scene() {
 					// diffuse
 					auto albedo = color::random() * color::random();
 					sphere_material = make_shared<lambertian>(albedo);
-					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+					auto center2 = center + vec3(0, random_double(0, 0.5), 0);
+					world.add(make_shared<moving_sphere>(
+						center,center2,0.0,1.0, 0.2, sphere_material));
 				}
 				else if (choose_mat < 0.95) {
 					// metal
@@ -97,10 +100,10 @@ hittable_list random_scene() {
 int main() {
 
 	// Image 
-	const auto aspect_ratio = 3.0 / 2.0;
-	const int image_width = 1200;
+	const auto aspect_ratio = 16.0/9.0;
+	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width/aspect_ratio);
-	const int samples_per_pixel = 500;
+	const int samples_per_pixel = 100;
 	const int max_depth = 50;
 
 	// World
@@ -115,7 +118,7 @@ int main() {
 	auto dist_to_focus = 10;
 	auto aperture = 0.1;
 
-	camera cam(lookfrom, lookat, vup,20, aspect_ratio,aperture,dist_to_focus);
+	camera cam(lookfrom, lookat, vup,20, aspect_ratio,aperture,dist_to_focus,0.0,1.0);
 
 
 	// Render
