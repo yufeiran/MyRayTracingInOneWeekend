4efd8b7e756da5602c875cf8504cfe1a0db6502e
yufeiran <yufeiran1999@gmail.com>
2022-09-07 15:22:59 +0800

    完成第5节 Perlin Noise 完成第6节 Image Texture Mapping

--- MyRayTracingInOneWeekend/main.cpp
+++ MyRayTracingInOneWeekend/main.cpp
@@ -47,6 +47,16 @@ color ray_color(const ray& r,const hittable&world,int depth) {
 
 }
 
+hittable_list two_perlin_spheres() {
+	hittable_list objects;
+
+	auto pertext = make_shared<noise_texture>(4);
+	objects.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(pertext)));
+	objects.add(make_shared<sphere>(point3(0, 2, 0), 2, make_shared<lambertian>(pertext)));
+
+	return objects;
+}
+
 hittable_list two_spheres() {
 	hittable_list objects;
 
@@ -111,6 +121,14 @@ hittable_list random_scene() {
 	return world;
 }
 
+hittable_list earth() {
+	auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+	auto earth_surface = make_shared<lambertian>(earth_texture);
+	auto globe = make_shared<sphere>(point3(0, 0, 0), 2, earth_surface);
+
+	return hittable_list(globe);
+}
+
 int main() {
 
 	// Image 
@@ -136,10 +154,22 @@ int main() {
 		vfov = 20.0;
 		aperture = 0.1;
 		break;
-	default:
 	case 2:
 		world = two_spheres();
 		lookfrom = point3(13, 2, 3);
+		lookat = point3(0, 0, 0);
+		vfov = 20.0;
+		break;
+	case 3:
+		world = two_perlin_spheres();
+		lookfrom = point3(13, 2, 3);
+		lookat = point3(0, 0, 0);
+		vfov = 20.0;
+		break;
+	default:
+	case 4:
+		world = earth();
+		lookfrom = point3(12, 2, 3);
 		lookat = point3(0, 0, 0);
 		vfov = 20.0;
 		break;
---
+++ MyRayTracingInOneWeekend/rtw_stb_image.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef RTWEEKEND_STB_IMAGE_H
+#define RTWEEKEND_STB_IMAGE_H
+
+#ifdef _MSC_VER
+#pragma warning (push,0)
+#endif
+
+#define STB_IMAGE_IMPLEMENTATION
+#include"external/stb_image.h"
+
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
+
+#endif // !RTWEEKEND_STB_IMAGE_H
