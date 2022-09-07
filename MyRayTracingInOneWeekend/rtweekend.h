e4f1b6f23008f51d62d19c099bafeb4bee4c26a2
yufeiran <yufeiran1999@gmail.com>
2022-09-07 09:32:48 +0800

    完成第4节 Solid Textures

--- MyRayTracingInOneWeekend/rtweekend.h
+++ MyRayTracingInOneWeekend/rtweekend.h
@@ -32,14 +32,16 @@ inline double random_double() {
 	//return distribution(generator);
 }
 
-inline int random_int(int min, int max) {
-	// Returns a random integer in [min,max].
-	return static_cast<int>(random_double(min, max + 1));
-}
+
 
 inline double random_double(double min, double max) {
 	// Returns a random real in [min,max).
 	return min + (max - min) * random_double();
+}
+
+inline int random_int(int min, int max) {
+	// Returns a random integer in [min,max].
+	return static_cast<int>(random_double(min, max + 1));
 }
 
 inline double clamp(double x, double min, double max) {
